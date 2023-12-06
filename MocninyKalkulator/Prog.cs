def0f896dd4ca1e75670af17554ec0f59da0c315
Matej Dolecek <1179@adc.itgymnazium.cz>
2023-12-06 11:48:56 +0100

    1.0

---
+++ MocninyKalkulator/Prog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MocninyKalkulator;
+
+internal class Prog
+{
+    public void Run()
+    {
+        bool c = true;
+        while (c)
+        {
+            Console.WriteLine("zadejte zaklad");
+            string a = Console.ReadLine();
+            if (a == "end")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("zadejte exponent");
+                string b = Console.ReadLine();
+                int cislo1 = ConvertToInt(a);
+                int cislo2 = ConvertToInt(b);
+                double vysledek = Math.Pow(cislo1, cislo2);
+                Console.WriteLine($"vysledek je {vysledek}");
+            }
+            
+        }
+       
+    }
+    public int ConvertToInt(string x)
+    {
+        if (int.TryParse(x, out int result))
+        {
+            return result;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
---
+++ MocninyKalkulator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MocninyKalkulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prog app = new Prog();
+            app.Run();
+        }
+    }
+}
